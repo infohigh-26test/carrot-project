69af38e77c4fe2c6c6050c6cdd0f3d645af874d8
GREEN <GREEN@G704-00>
2026-01-30 13:22:29 +0900

    project setting

---
+++ src/main/java/com/infohigh/InfoTestApplication.java
@@ -0,0 +1,13 @@
+package com.infohigh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infohigh/InfoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infohigh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
